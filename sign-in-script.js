ed7f0203a13a4dc76f45db2c0bf36dc16c784448
haze103 <112647323+haze103@users.noreply.github.com>
2024-06-05 01:49:34 +0800

    fix some functions in js

--- sign-in-script.js
+++ sign-in-script.js
@@ -1,9 +1,8 @@
-// Function to check login credentials
 function checkLoginCreds(strUserType) {
     var strStudentNumber = document.getElementById('txtUniqueIdentifier').value;
     var strPassword = document.getElementById('txtPassword').value;
 
-    if (strStudentNumber === 'yourValidStudentNumber' && strPassword === 'yourValidPassword') {
+    if (strStudentNumber === 'StudentNumber01' && strPassword === 'ValidPassword01') { // this will be edited after connecting with the db
         window.location.href = "home.html";
     } else if (strStudentNumber === '' || strPassword === '') {
         alert('Input field/s are empty.');
@@ -22,36 +21,55 @@ function generateVerCode() {
     const strChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let counter = 0;
     while (counter < 8) {
-        strCode += strChars.charAt(Math.floor(Math.random() * 8));
+        strCode += strChars.charAt(Math.floor(Math.random() * strChars.length));
         counter += 1;
     }
     return strCode;
 }
 
-var strGeneratedVerCode = generateVerCode();
+const strGeneratedVerCode = generateVerCode();
 
-function sendEmail() {
-    //send an email to the email address containing the generated verification code using generateVerCode() of the user stored in the database
-    strGeneratedVerCode;
+function confirmVerCode() {
+    var strVerificationCode = document.getElementById('txtVerCode').value;
+
+    if (strVerificationCode === strGeneratedVerCode) {
+        alert('Verification code confirmed. Please change your password in account settings.');
+        window.location.href = "home.html";
+    } else {
+        alert('Invalid verification code. Please try again.');
+    }
+}
+
+// error sending going to be fixed soon
+function sendOTP() {
+    // var strEmail = document.getElementById(''); => this is from the db/ this code will be edited
+    let strEmailBody = `<h2>Your OTP is: ${strGeneratedVerCode}</h2>`;
+
+    // got this code from https://smtpjs.com/ 
+    Email.send({
+        SecureToken: "bba39834-f516-40ed-a1b7-9eb109085a2b",
+        To : 'espiritu.mickaeladanna@gmail.com', // will be edited
+        From : "PUP-NALLRC",
+        Subject : "Here is your OTP",
+        Body : strEmailBody,
+    }).then(
+      message => {
+        if (message === "OK") {
+            alert("Your OTP has been sent to your email.");
+        } else {
+            alert("Error sending an OTP to your email.");
+        }
+      }
+    )
 }
 
 function showPopup() {
     document.querySelector('.shpContainer').classList.add('disabled');
     document.querySelector('.fsubPopup').style.display = 'flex';
+    sendOTP();
 }
   
 function closePopup() {
     document.querySelector('.shpContainer').classList.remove('disabled');
     document.querySelector('.fsubPopup').style.display = 'none';
-}
-
-  
-function confirmVerCode() {
-    var strVerificationCode = document.getElementById('txtVerCode').value;
-    if (strVerificationCode === strGeneratedVerCode) {
-        alert('Verification code confirmed. You can proceed.');
-        window.location.href = "home.html";
-    } else {
-        alert('Incorrect verification code. Please try again.');
-    }
 }
